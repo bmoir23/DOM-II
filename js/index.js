1958221f1136c7768bdc7cc353cb94c1a42ce465
bmoir23 <brianmoir80@gmail.com>
2019-09-09 15:03:33 -0400

    total update added another function

--- js/index.js
+++ js/index.js
@@ -1,30 +1,36 @@
 // Your code goes here
-// objects
-const headerImg = document.querySelector("header img");
+
 const introH = document.querySelector("header h2");
 const hiddenText = document.createElement("p");
     hiddenText.textContent = "This is hidden text! Look at you!";
     hiddenText.style.fontSize = "100px";
-const sectionHeaders = document.querySelectorAll(".text-content h2");
+
+const headerImg = document.querySelector("header img");
 const sectionContent = document.querySelectorAll(".text-content p");
-const links = document.querySelectorAll(".nav-link");
-const navBlocker = document.querySelector("nav");
-const destinSection = document.querySelector(".content-destination");
-const destinDiv = document.querySelector(".destination");
-
-
-// Events
 
 headerImg.addEventListener('dblclick', (event)=> {headerImg.style.display = "none"; introH.prepend(hiddenText);})
-// shows the super secret text when double clicking the header Image
+
+// changes header image super secret text when double clicking the header Image
+
+
+
+
+const sectionHeaders = document.querySelectorAll(".text-content h2");
 
 sectionHeaders[0].addEventListener('mouseover', (event) =>{sectionHeaders[0].style.color = "green";sectionHeaders[1].style.color = "green" });
 sectionHeaders[1].addEventListener('mouseover', (event) => {sectionContent[2].style.color = "purple"; sectionContent[3].style.color = "purple"});
-// changes headers green and second p purple
+
 
 sectionHeaders[0].addEventListener('mouseout', (event) => {sectionContent[0].style.color = "purple"; sectionContent[1].style.color = "purple"});
 sectionHeaders[1].addEventListener('mouseout', (event) => {sectionContent[2].style.color = "green"; sectionContent[3].style.color = "green"});
+// changes headers green and second p purple and inverts them when mouse over heading
 
+
+
+
+
+//  changes nav
+const links = document.querySelectorAll(".nav-link");
 
 links[0].addEventListener('focus', (event) => {links[0].style.fontSize = "40px"});
 links[1].addEventListener('focus', (event) => {links[1].style.fontSize = "40px"});
@@ -39,7 +45,15 @@ links[3].addEventListener('blur', (event) => {links[3].style.fontSize = "1.6rem"
 //  reverts nav tags back to normal when clicking the next tag
 
 
+// after doing a bunch of studying online I found a few different ways to change the DOM
+let keyPress = document.querySelector('body').addEventListener("keydown", keyFunction);
 
+
+function keyFunction(){
+    this.style.backgroundColor = "grey";
+}
+
+// changes background to grey when a key is pressed
 
 
 
